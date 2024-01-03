a4d3461f83026ee8e04590cda918c74147672b3c
jeremiarf03 <jeremiarfnto@gmail.com>
2024-01-03 20:19:52 +0700

    revisi

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import Table from "./table.js";
+
+const table = new Table({
+    columns: ["Name", "Email"],
+    data: [
+        ["Jeremia Arfianto", "jeremiarfnto@gmail.com"],
+        ["Jubaedah Sarutobi", "jubaedahgemink23@gmail.com"]
+    ]
+  });
+  
+const app = document.getElementById("app");
+table.render(app);
+
--- table.js
+++ table.js
@@ -39,15 +39,16 @@ class Table {
   }
 }
 
-const table = new Table({
-  columns: ["Name", "Email"],
-  data: [
-      ["Jeremia Arfianto", "jeremiarfnto@gmail.com"],
-      ["Jubaedah Sarutobi", "jubaedahgemink23@gmail.com"]
-  ]
-});
+// const table = new Table({
+//   columns: ["Name", "Email"],
+//   data: [
+//       ["Jeremia Arfianto", "jeremiarfnto@gmail.com"],
+//       ["Jubaedah Sarutobi", "jubaedahgemink23@gmail.com"]
+//   ]
+// });
 
-const app = document.getElementById("app");
-table.render(app);
+// const app = document.getElementById("app");
+// table.render(app);
 
 export default Table;
+
