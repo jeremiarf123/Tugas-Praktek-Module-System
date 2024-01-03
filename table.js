d1a2809f46cc78b98206e0d500e273f5ede446ad
jeremiarf03 <jeremiarfnto@gmail.com>
2024-01-03 20:21:51 +0700

    fixed

--- table.js
+++ table.js
@@ -39,16 +39,5 @@ class Table {
   }
 }
 
-// const table = new Table({
-//   columns: ["Name", "Email"],
-//   data: [
-//       ["Jeremia Arfianto", "jeremiarfnto@gmail.com"],
-//       ["Jubaedah Sarutobi", "jubaedahgemink23@gmail.com"]
-//   ]
-// });
-
-// const app = document.getElementById("app");
-// table.render(app);
-
 export default Table;
 
